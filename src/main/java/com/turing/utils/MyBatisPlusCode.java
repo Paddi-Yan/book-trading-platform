79a3cf09da9a41201fcd22b11b7e9885e72751ee
qds <1956392621@qq.com>
2022-01-26 22:51:48 +0800

    准备代码生成前存档

---
+++ src/main/java/com/turing/utils/MyBatisPlusCode.java
@@ -0,0 +1,34 @@
+package com.turing.utils;
+
+
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.util.Collections;
+
+public class MyBatisPlusCode {
+    public static void main(String[] args) {
+        FastAutoGenerator.create("jdbc:mysql://localhost:3306/sys?useUnicode=true&characterEncoding=utf-8&useSSL=false",
+                        "root",
+                        "123456")
+                .globalConfig(builder -> {
+                    builder.author("qds") // 设置作者
+                            .enableSwagger() // 开启 swagger 模式
+                            .fileOverride() // 覆盖已生成文件
+                            .outputDir("I:\\ProjectAll\\work\\wechat-mini-server\\src\\main\\java"); // 指定输出目录
+                })
+                .packageConfig(builder -> {
+                    builder.parent("com.turing") // 设置父包名
+                            .moduleName("entity") // 设置父包模块名
+                            .pathInfo(Collections.singletonMap(OutputFile.mapperXml, "I:\\ProjectAll\\work\\wechat-mini-server\\src\\main\\resources\\mapper")); // 设置mapperXml生成路径
+                })
+                .strategyConfig(builder -> {
+                    builder.addInclude("sys") // 设置需要生成的表名
+                            .addTablePrefix("t_", "c_"); // 设置过滤表前缀
+                })
+                .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
+                .execute();
+
+    }
+}
